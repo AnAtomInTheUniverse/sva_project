b13515b35a33107206db9ea8160b6b1c0a8c0bbd
Amlan Nayak <amlan@umich.edu>
2015-10-11 20:43:34 -0400

    +3 assertions.  still not working. Arbiter passes. All Assertions pass

--- arbiter/test_arbiter.sv
+++ arbiter/test_arbiter.sv
@@ -37,6 +37,7 @@ module test_arbiter;
     @(posedge clock);*/
     reset = 1'b0;
 
+	repeat(2) @(posedge clock);
     // Carrying out directed tests with known input requests
     // Baseline condition is when there are no requests
     // In this case the arbiter could keep granting to
@@ -44,12 +45,9 @@ module test_arbiter;
     request     = 'b0;
     last_req    = 'b0;  
 
-    $display("---------- Both Requests Deasserted ----------");
+    @(negedge clock) $display("---------- Both Requests Deasserted ----------");
     // See output grant for 6 cycles
-    for(int j = 0; j < 6; j++) begin
-        $display("grant = %b", grant);
-        @(posedge clk);
-    end
+    repeat(6) @(posedge clock)
     
     // Check the condition when only request[0] is asserted
     // for multiple cycles
@@ -59,7 +57,7 @@ module test_arbiter;
     // See output grant for 6 cycles
     for(int j = 0; j < 6; j++) begin
         $display("grant = %b", grant);
-        @(posedge clk);
+        @(posedge clock);
     end
 
     // In this section we alternate between the two requests
@@ -70,9 +68,7 @@ module test_arbiter;
     $display("---------- Alternating Requests ----------");
     for(int i = 0; i < 10; i++) begin
        request  = last_req;
-       repeat(2) @(posedge clk) ;
-       assert 
-       $display("grant = %b", grant);
+       repeat(2) @(posedge clock) ;
        last_req =   {~request[1], ~request[0]};
     end
 
@@ -80,16 +76,18 @@ module test_arbiter;
     // vector and check the corresponding grants
     $display("---------- Randomizing Requests ----------");
     for(int i = 0; i < 20; i++) begin
-        request = std::randomize(rand_req);
-        repeat(2) @(posedge clk) ;
-        $display("grant = %b", grant);
+        request = $random;
+        repeat(2) @(posedge clock) ;
     end
 
     // In this section both of the request inputs are asserted
     // The arbiter must fairly grant to both requests, one at a time
     $display("---------- Both Requests Asserted ----------");
     request = 2'b11;
-    repeat(20) @(posedge clk) $display("grant = %b", grant);
+    repeat(20) @(posedge clock) $display("grant = %b", grant);
+	
+	// Randomizing for the remaining cycles
+	repeat(200) @(posedge clock) request = $random;
     // terminate simulation
     // FIXME: change simulation time if necessary
     #10000 $finish;
@@ -100,6 +98,9 @@ module test_arbiter;
     clock = ~clock;
   end
 
+  always @(negedge clock) begin
+		  $display("grant = %d", grant);
+  end
   ////////////////////////////////////////////////////////////////////
   // request signal generation
   ////////////////////////////////////////////////////////////////////
@@ -107,9 +108,9 @@ module test_arbiter;
   // The below always statement generates a random number every cycle and
   // assign the number to request.
   //
- // always @(posedge clock) begin
- //   request = $random;
- // end
+  //always @(posedge clock) begin
+  //  request = $random;
+  //end
 
   ////////////////////////////////////////////////////////////////////
   // assertions
@@ -122,14 +123,25 @@ module test_arbiter;
   //
 
   // Only one of the two requests can be granted at a time - both
-  // bits of grant[1:0] cannot be asserted simultaneously
-  prop_mutex: assert property (@(posedge clock) disable iff (reset) !(grant[0] == grant[1]));
+  // bits of grant[1:0] cannot be asserted simultaneously (mutual exclusion)
+  prop_mutex: assert property (@(posedge clock) disable iff (reset) !(grant[0] && grant[1]));
 
   // If request[0] is asserted and request[1] is not, then the 
   // arbiter must grant to 0 in the next cycle
-  prop_gnt_0: assert property (@(posedge clock) disable iff (reset) (~request[1] && request[0] |-> ##1 grant == 2'b01);
+  prop_gnt_0: assert property (@(posedge clock) disable iff (reset) (!request[1] && request[0]) |-> ##1 grant == 2'b01);
 
   // If request[1] is asserted and request[0] is not, then the 
   // arbiter must grant to 1 in the next cycle
-  prop_gnt_1: assert property (@(posedge clock) disable iff (reset) (request[1] && ~request[1] |-> ##1 grant == 2'b10);
+  prop_gnt_1: assert property (@(posedge clock) disable iff (reset) (request[1] && ~request[0]) |-> ##1 grant == 2'b10);
+
+  // Test fairness: if both requests are asserted for an extended 
+  // number of cycles, then the arbiter must grant to both fairly
+  // by alternating between the two
+  prop_fair: assert property (@(posedge clock) disable iff (reset)  (grant[0]) ##1 (request == 2'b11) |=> grant[1]);
+
+  // The arbiter should not grant incorrectly - if an input is not requesting, 
+  // then the arbiter should not grant that request line
+  prop_correct0: assert property (@(posedge clock) disable iff (reset) (!request[0] |=> grant[0] != 1));
+  prop_correct1: assert property (@(posedge clock) disable iff (reset) (!request[1] |=> grant[1] != 1));
+
 endmodule
