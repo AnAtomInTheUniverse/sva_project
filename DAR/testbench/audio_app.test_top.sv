78b3ff4e2e5c5b7f2d08edf753f6520593898860
Amlan Nayak <amlan@umich.edu>
2015-10-25 20:37:44 -0400

    mostly done. All of testplan has been covered. Implemented reprogramming of delay checks.

--- DAR/testbench/audio_app.test_top.sv
+++ DAR/testbench/audio_app.test_top.sv
@@ -23,9 +23,24 @@ module audio_app_test_top;
   //--------------------
   // Testbench temp vars
   //--------------------
-  logic [2:0]   tap_val [4];
+  logic [2:0]   tap_val_rpt [4];
+  logic [2:0]   tap_val_drop [4];
   logic [1:0]	dl;
   logic [2:0]	delay_val;
+  logic			forked_0 = 1'b0;;
+  logic			forked_1 = 1'b0;;
+  logic			forked_2 = 1'b0;;
+  logic			forked_3 = 1'b0;;
+
+  logic			forked_0_drop = 1'b0;;
+  logic			forked_1_drop = 1'b0;;
+  logic			forked_2_drop = 1'b0;;
+  logic			forked_3_drop = 1'b0;;
+
+  logic			forked_0_tap_4 = 1'b0;;
+  logic			forked_1_tap_4 = 1'b0;;
+  logic			forked_2_tap_4 = 1'b0;;
+  logic			forked_3_tap_4 = 1'b0;;
 
   audio_app dut(
     .clk  ( clk ),
@@ -63,8 +78,15 @@ module audio_app_test_top;
     @(negedge SystemClock);
     prgrm_go_	=	1'b1;
 	prgrm_in	=	1'b0;
-	tap_val[delay_line]	=	delay_amt;
   endtask
+  
+  task reset_all;
+	@(negedge SystemClock);
+	SystemReset_	=	1'b0;
+	repeat(2) @(posedge SystemClock);
+	@(negedge SystemClock);
+	SystemReset_	=	1'b1;
+  endtask	
   
   initial begin
     SystemClock = 0 ;
@@ -103,12 +125,7 @@ module audio_app_test_top;
 
 	repeat(6) @(posedge SystemClock);
 
-	@(negedge SystemClock);
-	SystemReset_	=	1'b0;
-	
-	repeat(2) @(posedge SystemClock);
-	@(negedge SystemClock);
-	SystemReset_	=	1'b1;
+	reset_all();
 	@(posedge SystemClock);
 	move_tap(1'b0, 3'b111, 2'b00);
 	move_tap(1'b0, 3'b111, 2'b01);
@@ -118,13 +135,7 @@ module audio_app_test_top;
 	// -----------------
 	// Delay Tap Testing 
 	// -----------------
-	@(negedge SystemClock);
-	SystemReset_	=	1'b0;
-	
-	repeat(2) @(posedge SystemClock);
-
-	@(negedge SystemClock)
-	SystemReset_	=	1'b1;
+	reset_all();
 	move_tap(1'b0, 3'b111, 2'b00);
 
 	@(negedge SystemClock);
@@ -138,13 +149,7 @@ module audio_app_test_top;
 	// delay line 0, set delay tap to 7 and then 
 	// check that the output appears exactly 8 cycles
 	// later. 
-	@(negedge SystemClock);
-	SystemReset_	=	1'b0;
-
-	repeat(2) @(posedge SystemClock);
-
-	@(negedge SystemClock);
-	SystemReset_	=	1'b1;
+	reset_all();
 	di_0 			=	16'h00_00;
 	move_tap(1'b0, 3'b111, 2'b00);
 	for(int i = 0; i < 16; i++) begin
@@ -160,13 +165,7 @@ module audio_app_test_top;
 	// delay lines - setting the tap to a certain value
 	// for a line should NOT affect the delay of another
 	// line. 
-	@(negedge SystemClock);
-	SystemReset_	=	1'b0;
-
-	repeat(2) @(posedge SystemClock);
-
-	@(negedge SystemClock);
-	SystemReset_	=	1'b1;
+	reset_all();
 	di_0			=	16'h00_00;
 	di_1			=	16'h00_00;
 	di_2			=	16'h00_00;
@@ -197,16 +196,15 @@ module audio_app_test_top;
 	dl_3: assert(do_3 == 16'hFF_FF) else $display("Incorrect delay on line 3!");
 
 	
-	// Cycle through all delay tap values (0 -> 7)
-	// for each delay line and check whether the output
-	// delay matches the programmed delay
-	@(negedge SystemClock);
-	SystemReset_	=	1'b0;
-
-	repeat(2) @(posedge SystemClock);
-
-	@(negedge SystemClock);
-	SystemReset_	=	1'b1;
+	// Cycle through random delay tap values for
+	// each delay line and check whether the output
+	// delay matches the programmed delay by comparing
+	// the 16 bit random output value exactly delay+1
+	// cycles after it was sent in as input. Note that
+	// the checking is done delay+2 cycles later as the 
+	// assertion checks the prior value of a reg at the 
+	// posedge clock transition point 
+	reset_all();
 	for(int i = 0; i < 4 ; i++)	begin
 		dl	=	(i == 0) ? 2'b00 : (i == 1) ? 2'b01 : (i == 2) ? 2'b10 : 2'b11;
 		for(int j = 0; j < 50; j++) begin
@@ -230,40 +228,207 @@ module audio_app_test_top;
 				di_3			=	$random;
 			end
 
-			repeat(delay_val+2) @(posedge SystemClock);
+			/* We perform two checks at the transition
+			* point - the first assertion triggers exactly
+			* delay+1 cycles after the input was changed but
+			* the value has not transitioned yet - so the 
+			* output should be the old value*/
+			repeat(delay_val+1) @(posedge SystemClock);
 			if(i == 0)
-				rand_dl_0:assert(do_0	==	di_0) else $display("Delay line 0 failed random delay test");
+				rand_dl_0_old_val:assert(do_0	!=	di_0) else $display("Delay line 0 failed random delay test");
 			else if(i == 1)
-				rand_dl_1:assert(do_1	==	di_1) else $display("Delay line 0 failed random delay test");
+				rand_dl_1_old_val:assert(do_1	!=	di_1) else $display("Delay line 0 failed random delay test");
 			else if(i == 2)
-				rand_dl_2:assert(do_2	==	di_2) else $display("Delay line 0 failed random delay test");
+				rand_dl_2_old_val:assert(do_2	!=	di_2) else $display("Delay line 0 failed random delay test");
 			else if(i == 3)
-				rand_dl_3:assert(do_3	==	di_3) else $display("Delay line 0 failed random delay test");
+				rand_dl_3_old_val:assert(do_3	!=	di_3) else $display("Delay line 0 failed random delay test");
+
+			/* The output should have transitioned to the
+			* new value at the next edge so we assert that
+			* the ouput is the new value that was programmed
+			* in with delay delay_val*/
+			@(posedge SystemClock);
+			if(i == 0)
+				rand_dl_0_new_val:assert(do_0	==	di_0) else $display("Delay line 0 failed random delay test");
+			else if(i == 1)
+				rand_dl_1_new_val:assert(do_1	==	di_1) else $display("Delay line 0 failed random delay test");
+			else if(i == 2)
+				rand_dl_2_new_val:assert(do_2	==	di_2) else $display("Delay line 0 failed random delay test");
+			else if(i == 3)
+				rand_dl_3_new_val:assert(do_3	==	di_3) else $display("Delay line 0 failed random delay test");
 		end
 	end
 
-//	delay_check: assert property (@(posedge SystemClock) (do_0 == $past(di_0, dl_0_tap))) else $display("FAIL!");
+	//--------------------------------------------
+	// Testing Data word drop due to Reprogramming
+	// -------------------------------------------
+	/* In this section we test the correct reprogrammability
+	* of the delay tap in each line. Given an input sequence 
+	* of known data values, if the tap is reprogrammed during
+	* the sequence, the number of data words dropped should
+	* be a function of the new and old positions of the delay
+	* tap (MAX DROP = 7, MAX REPEAT = 7) 
 	
+	* Feed in data words starting at random values and incrementing
+	* by 1 each cycle. Then, we move the tap by a specific amount
+	* and assert that the output after the reprogramming is complete
+	* must be the value of the input from 7 cycles prior*/
+	reset_all();
 
+	di_0	=	$random;	//16'h00_00;
+	di_1	=	$random;	//16'h00_00;
+	di_2	=	$random;	//16'h00_00;
+	di_3	=	$random;	//16'h00_00;
+	fork begin
+		for(int i = 0; i < 50; i++) begin
+			@(negedge SystemClock);
+			di_0	+=	 1;
+			di_1	+=	 1;
+			di_2	+=	 1;
+			di_3	+=	 1;
+		end
+	end
+	begin
+		delay_val	=	3'b111;
+		move_tap(1'b0, delay_val, 2'b00);
+		forked_0 = 1'b1;
+		move_tap(1'b0, delay_val, 2'b01);
+		forked_1 = 1'b1;
+		move_tap(1'b0, delay_val, 2'b10);
+		forked_2 = 1'b1;
+		move_tap(1'b0, delay_val, 2'b11);
+		forked_3 = 1'b1;
+	end
+	
+	join
+	forked_0 = 1'b0;
+	forked_1 = 1'b0;
+	forked_2 = 1'b0;
+	forked_3 = 1'b0;
 
+	/* Now we test the opposite case - moving the tap 
+	* from 7 to 0. We should see no more than 7 words 
+	* of data dropped.*/
+	repeat(10) @(posedge SystemClock);
+	reset_all();
+	// Programming the taps to 7
+	move_tap(1'b0, 3'b111, 2'b00);
+	move_tap(1'b0, 3'b111, 2'b01);
+	move_tap(1'b0, 3'b111, 2'b10);
+	move_tap(1'b0, 3'b111, 2'b11);
+	
+	di_0	=	$random;//16'h00_00;
+	di_1	=	$random;//16'h00_00;
+	di_2	=	$random;//16'h00_00;
+	di_3	=	$random;//16'h00_00;
+	fork begin
+		for(int i = 0; i < 50; i++) begin
+			@(negedge SystemClock);
+			di_0	+=	 1;
+			di_1	+=	 1;
+			di_2	+=	 1;
+			di_3	+=	 1;
+		end
+	end
+	begin
+		delay_val	=	3'd0;
+		move_tap(1'b0, delay_val, 2'b00);
+		forked_0_drop = 1'b1;
+		move_tap(1'b0, delay_val, 2'b01);
+		forked_1_drop = 1'b1;
+		move_tap(1'b0, delay_val, 2'b10);
+		forked_2_drop = 1'b1;
+		move_tap(1'b0, delay_val, 2'b11);
+		forked_3_drop = 1'b1;
+	end
+	
+	join
+	forked_0_drop = 1'b0;
+	forked_1_drop = 1'b0;
+	forked_2_drop = 1'b0;
+	forked_3_drop = 1'b0;
+
+	repeat(10) @(posedge SystemClock);
+
+	/* Now we choose a different delay tap for
+	* each line, reprogram, and check whether 
+	* the number of repeated packets matches the 
+	* expected number given the delay for each 
+	* line. All lines start with delay of 7 and 
+	* transition to a different value.
+	* **NOTE**: We DO NOT change the delay tap
+	* for line 3 but WE DO reprogram it. There
+	* should be no dropped packets in this case.*/
+	reset_all();
+	// Programming the taps to 7
+	move_tap(1'b0, 3'b111, 2'b00);
+	move_tap(1'b0, 3'b111, 2'b01);
+	move_tap(1'b0, 3'b111, 2'b10);
+	move_tap(1'b0, 3'b000, 2'b11);
+	
+	di_0	=	16'h00_00;
+	di_1	=	16'h00_00;
+	di_2	=	16'h00_00;
+	di_3	=	16'h00_00;
+	fork begin
+		for(int i = 0; i < 50; i++) begin
+			@(negedge SystemClock);
+			di_0	+=	 1;
+			di_1	+=	 1;
+			di_2	+=	 1;
+			di_3	+=	 1;
+		end
+	end
+	begin
+		move_tap(1'b0, 3'd4, 2'b00);
+		forked_0_tap_4 = 1'b1;
+		move_tap(1'b0, 3'd2, 2'b01);
+		forked_1_tap_4 = 1'b1;
+		move_tap(1'b0, 3'd6, 2'b10);
+		forked_2_tap_4 = 1'b1;
+		move_tap(1'b0, 3'd0, 2'b11);
+		forked_3_tap_4 = 1'b1;
+	end
+	
+	join
+	forked_0_tap_4 = 1'b0;
+	forked_1_tap_4 = 1'b0;
+	forked_2_tap_4 = 1'b0;
+	forked_3_tap_4 = 1'b0;
 
 	//-------------------------
-	// Testing READ Mode Error
+	// Testing err_ signal 
 	//-------------------------
 	// Try programming in read mode and 
 	// check to see if err_ signal is asserted
-	@(negedge SystemClock);
-	SystemReset_	=	1'b0;
-
-	repeat(2) @(posedge SystemClock);
-	
-	@(negedge SystemClock);
-	SystemReset_	=	1'b1;
+	reset_all();
 	move_tap(1'b1, 3'b010, 2'b00);
 
-    // terminate simulation
-    // FIXME: change simulation time if necessary
-    #40000 $finish;
+	/* Deassert prgrm_go_ prematurely and
+	* see if err_ is asserted*/
+	reset_all();
+    @(negedge SystemClock);
+    prgrm_go_	=	1'b0;
+    prgrm_in	=	1'b0;
+    @(negedge SystemClock);
+    prgrm_in	=	1'b0;
+    @(negedge SystemClock);
+    prgrm_in	=	1'b0;
+	// Deasserting prgrm_go_ - err_ should be asserted
+	prgrm_go_	=	1'b1;
+    @(negedge SystemClock);
+    prgrm_in	=	1'b1;
+    @(negedge SystemClock);
+    prgrm_in	=	1'b1;
+    @(negedge SystemClock);
+    prgrm_in	=	1'b1;
+    @(negedge SystemClock);
+    prgrm_go_	=	1'b1;
+	prgrm_in	=	1'b0;
+
+	reset_all();
+	//Terminate Simulation
+    #50000 $finish;
   end
   
   //initial begin
@@ -275,9 +440,9 @@ module audio_app_test_top;
   // CREATE YOUR TESTBENCH BELOW
   //
 
-  //----------------------------
-  //		 ASSERTIONS
-  //----------------------------
+  //--------------------------------
+  //	 CONCURRENT ASSERTIONS
+  //--------------------------------
   
   //----------------
   //Reset Assertions
@@ -303,8 +468,39 @@ module audio_app_test_top;
   //------------------------
   //Program Delay Assertions
   //------------------------
-  
+  /* Checking that only 7 words are repeated*/
+  move_tap_0_7_0: assert property (@(negedge SystemClock) 
+  										disable iff (!SystemReset_) (forked_0 |-> (do_0 == $past(di_0, 7))));
+  move_tap_0_7_1: assert property (@(negedge SystemClock) 
+  										disable iff (!SystemReset_) (forked_1 |-> (do_1 == $past(di_1, 7))));
+  move_tap_0_7_2: assert property (@(negedge SystemClock) 
+  										disable iff (!SystemReset_) (forked_2 |-> (do_2 == $past(di_2, 7))));
+  move_tap_0_7_3: assert property (@(negedge SystemClock) 
+  										disable iff (!SystemReset_) (forked_3 |-> (do_3 == $past(di_3, 7))));
+  /* Checking that no more than 7 words are dropped*/
+  move_tap_7_0_0: assert property (@(negedge SystemClock) 
+  		     						disable iff (!SystemReset_) (forked_0_drop |-> (do_0 == di_0)));
+  move_tap_7_0_1: assert property (@(negedge SystemClock) 
+  		     						disable iff (!SystemReset_) (forked_1_drop |-> ((do_1 - $past(do_1)) == 8)||(do_1 == di_1)));
+  move_tap_7_0_2: assert property (@(negedge SystemClock) 
+  		     						disable iff (!SystemReset_) (forked_2_drop |-> ((do_2 - $past(do_2)) == 8)||(do_2 == di_2)));
+  move_tap_7_0_3: assert property (@(negedge SystemClock) 
+  									disable iff (!SystemReset_) (forked_3_drop |-> ((do_3 - $past(do_3)) == 8)||(do_3 == di_3)));
 
+  /* Checking that the number of packets dropped when delay
+  * tap is reprogrammed to 4 from 7 is 3. We do this by taking 
+  * the difference between the output value for each delay line 
+  * at every cycle after reprogramming and making sure that 
+  * the value is 4 (since the inputs are increasing by a value 
+  * of 1 every cycle and 3 words are dropped due to reprogramming)*/
+  move_tap_0_7_4: assert property (@(negedge SystemClock) 
+  			 			disable iff (!SystemReset_) (forked_0_tap_4 |-> (do_0 == di_0 - 16'd4)));
+  move_tap_1_7_4: assert property (@(negedge SystemClock) 
+  			 			disable iff (!SystemReset_) (forked_1_tap_4 |-> (do_1 == di_1 - 16'd2)));
+  move_tap_2_7_4: assert property (@(negedge SystemClock) 
+  			 			disable iff (!SystemReset_) (forked_2_tap_4 |-> (do_2 == di_2 - 16'd6)));
+  move_tap_3_7_4: assert property (@(negedge SystemClock) 
+  						disable iff (!SystemReset_) (forked_3_tap_4 |-> (do_3 == do_3)));
 
 
 
